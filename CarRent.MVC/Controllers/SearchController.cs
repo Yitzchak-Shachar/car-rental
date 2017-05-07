d108ee8062dc50e1dda71cdc9f8938e594dfa31e
ysh443 <ysh443@gmail.com>
2017-05-07 21:14:29 +0300

    set all selects to one row

--- CarRent.MVC/Controllers/SearchController.cs
+++ CarRent.MVC/Controllers/SearchController.cs
@@ -11,18 +11,23 @@ namespace CarRent.MVC.Controllers
 {
     public class SearchController : Controller
     {
-        private CarManager carManager;
-
+        private CarManager carMgr;
+        private CarTypeManager carTypeMgr;
         public SearchController()
         {
-            carManager = new CarManager();
+            carMgr = new CarManager();
+      carTypeMgr = new CarTypeManager();
         }
 
 
         // GET: Search
         public ActionResult Index()
         {
-           var allCars=  carManager.GetCars().Select(car=>new CarVM(car));
+           var allCars=  carMgr.GetCars().Select(car=>new CarVM(car));
+            var allCarTypes = carTypeMgr.GetCarTypes();//.Select(ctm => ctm.Manufacture);
+            List<SelectListItem> listCarTypes = new List<SelectListItem>();
+            listCarTypes.AddRange(allCarTypes.Select(ct => new SelectListItem() { Text = ct.Model, Value = ct.CarTypeId.ToString() }));
+            ViewBag.listOfCarTypes = listCarTypes;
 
             return View(allCars);
         }
